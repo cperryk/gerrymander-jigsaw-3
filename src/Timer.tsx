b1019db9a8e3260d6d48c62c87f055124d5a2082
Christopher Kirk <chriskirk@Christophers-MacBook-Pro.local>
2020-01-27 23:44:10 -0500

    Shrink top bar

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -4,7 +4,7 @@ import { formatTime } from "./utils";
 export const Timer: React.FunctionComponent<{ time: number }> = ({ time }) => {
   return (
     <div className="timer">
-      <span className="time">{formatTime(time)}</span>
+      Time: <span className="time">{formatTime(time)}</span>
     </div>
   );
 };
