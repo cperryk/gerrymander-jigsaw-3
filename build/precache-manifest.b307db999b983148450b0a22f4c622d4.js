d152891e27f2eda3d2ee3d372f8954b5aae70e89
Christopher Kirk <chriskirk@Christophers-MacBook-Pro.local>
2020-01-27 23:49:12 -0500

    Rework GA positions, rebuild

--- build/precache-manifest.b307db999b983148450b0a22f4c622d4.js
+++ build/precache-manifest.b307db999b983148450b0a22f4c622d4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bda7faca97d4e51ac03e4f0f54c531fc",
+    "revision": "f40c6be3305a68a49537bf076c1bb2fe",
     "url": "/gerrymander-jigsaw-3/build/index.html"
   },
   {
-    "revision": "64ec66e29c2e114cfcb8",
-    "url": "/gerrymander-jigsaw-3/build/static/css/main.cf74506c.chunk.css"
+    "revision": "102d90b70b297d52cffc",
+    "url": "/gerrymander-jigsaw-3/build/static/css/main.fad081f5.chunk.css"
   },
   {
     "revision": "0432892a973bc9390d72",
     "url": "/gerrymander-jigsaw-3/build/static/js/2.2adb927a.chunk.js"
   },
   {
-    "revision": "64ec66e29c2e114cfcb8",
-    "url": "/gerrymander-jigsaw-3/build/static/js/main.6f3b5b9a.chunk.js"
+    "revision": "102d90b70b297d52cffc",
+    "url": "/gerrymander-jigsaw-3/build/static/js/main.2544325f.chunk.js"
   },
   {
     "revision": "694f349c6befcae0a17f",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gerrymander-jigsaw-3/build/precache-manifest.f32844a197274ee8ecd2e9e716aaab2f.js"
+  "/gerrymander-jigsaw-3/build/precache-manifest.b307db999b983148450b0a22f4c622d4.js"
 );
 
 self.addEventListener('message', (event) => {
